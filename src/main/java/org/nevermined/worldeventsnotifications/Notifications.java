2cf9c55b92af080137c2e91383749bb17da1bc9b
Wyne <izmodenov1997@gmail.com>
2024-08-28 17:25:12 +0500

    Initial commit

---
+++ src/main/java/org/nevermined/worldeventsnotifications/Notifications.java
@@ -0,0 +1,17 @@
+package org.nevermined.worldeventsnotifications;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Notifications extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
